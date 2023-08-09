abefe2e82ce5315b0fc86e16d017154cdbf30438
Azriel <azrihell.l@gmail.com>
2023-08-08 22:26:18 -0500

    contact form outline completed

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -44,6 +44,35 @@ const Contact = () => {
               className='bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outlined-none border-none font-medium'
             />
           </label>
+
+          <label className='flex flex-col'>
+            <span className='text-white font-medium mb-4'>Your Email</span>
+            <input
+              type='email'
+              name='email'
+              value={form.email}
+              onChange={handleChange}
+              placeholder="What's your email?"
+              className='bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outlined-none border-none font-medium'
+            />
+          </label>
+
+          <label className='flex flex-col'>
+            <span className='text-white font-medium mb-4'>Your Message</span>
+            <textarea
+              rows='7'
+              name='message'
+              value={form.message}
+              onChange={handleChange}
+              placeholder="What do you want to say?"
+              className='bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outlined-none border-none font-medium'
+            />
+          </label>
+
+          <button type='submit' className='bg-tertiary py-3 px-8 hover:bg-secondary hover:text-primary outline-none w-fit text-white font-bold shadow-md shadow-primary rounded-xl'>
+            {loading ? 'Sending...' : 'Send'}
+          </button>
+
         </form>
       </motion.div>
     </div>
