50011e28b49c43de979dfb597eb33d73ea960af7
Azriel <azrihell.l@gmail.com>
2023-08-08 23:24:59 -0500

    tech balls now auto move

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -5,7 +5,6 @@ import { SectionWrapper } from "../hoc"
 import { technologies } from "../constants"
 
 const Tech = () => {
-  
   return (
     <div className='flex flex-row flex-wrap justify-center gap-10'>
       {technologies.map((technology) => (
@@ -17,4 +16,4 @@ const Tech = () => {
   )
 }
 
-export default SectionWrapper(Tech, "");
+export default SectionWrapper(Tech, "")
