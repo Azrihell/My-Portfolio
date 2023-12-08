52b58061d0092922920b01dc27fb0248a6b3c3d4
Azriel <azrihell.l@gmail.com>
2023-12-08 13:21:07 -0600

    vercel analytics test

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter } from "react-router-dom"
 import { About, Contact, Certificates, Feedbacks, Hero, Navbar, Tech, Projects, StarsCanvas } from './components'
+import { Analytics } from '@vercel/analytics/react';
 
 
 const App = () => {
 
   return (
     <BrowserRouter>
+    <Analytics />
       <div className='relative z-0 bg-primary'>
         <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center'>
           <Navbar />
